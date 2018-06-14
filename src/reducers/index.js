e3f235883c239d7f57a0aaa4aa237af57e413a31
SHAN SHAJI <shanshaji@gmail.com>
2018-06-14 12:27:05 +0530

    react-redux electron boilerplate

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux';
 
-import TheatreReducer from './theatres/theatre-reducer';
 
 const rootReducer = combineReducers({
-    theatre: TheatreReducer,
+   
 });
 
 export default rootReducer;
